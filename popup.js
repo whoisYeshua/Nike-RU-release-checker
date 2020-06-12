41d2a2be39015db8cb8b90f52afc1e4707ce5001
“whoisYeshua” <alien1998@yandex.ru>
2020-06-12 20:57:25 +0300

    add style & localStorage support

--- popup.js
+++ popup.js
@@ -1,18 +1,21 @@
 function getProductId(url) {
-    return new Promise(function(resolve, reject)  {
+    return new Promise(function(resolve)  {
         let xhr = new XMLHttpRequest();
 
 
         xhr.open("GET", url);
         xhr.send();
         xhr.onload = function () {
-            console.log(`Загружено: ${xhr.status}`);
             if (xhr.status === 200) {
+                console.log('%c%s','color: white; background: green; font: 16px sans-serif; padding: 5px;',xhr.status);
                 resolve(xhr.response)
             } else {
-                reject(xhr.status)
+                console.log('%c%s','color: white; background: red; font: 16px sans-serif; padding: 5px;',xhr.status)
             }
         };
+        xhr.onerror = function() {
+            console.log('Error in request`s creation')
+        }
     })
 }
 
@@ -25,44 +28,72 @@ url.searchParams.append('filter','channelId(010794e5-35fe-4e32-aaff-cd2c74f89d61
 
 
 
-let now  = new Date()
-console.log(now.toISOString());
+
 
 getProductId(url.href).then(function(result) {
     let data = JSON.parse(result)
-    
-    for (let release of data.objects) {
-        if (release.productInfo) {
-            console.group(`${release.publishedContent.properties.seo.slug}`)
-            console.log(release.publishedContent.properties.coverCard.properties.title)
-            
-            
-                for (let product of release.productInfo) {
-                    
-                    console.groupCollapsed(product.merchProduct.labelName)
-                    
-                    console.log(product.imageUrls.productImageUrl)
-                    console.log(product.merchProduct.id)
-        
-                    console.groupCollapsed('sku - size')
-        
-                    for (let item of  product.skus) {
-                        console.log(item.nikeSize, ' - ',item.id)
-                    }
-                    console.groupEnd()
-        
-                    console.groupCollapsed('sku - stock')
-        
-                    for (let stock of product.availableSkus) {
-                        console.log(stock.skuId, ' - ', stock.level); 
-                    }
-                    console.groupEnd()
-                    console.groupEnd()
-                }
-            }
-            console.groupEnd()
-        }
+    let now  = new Date()
+
+
+    let store = {
+        lastFetch: new Date().toISOString(),
+        slug: []
     }
-)
+    data.objects.forEach((release)=> {
+        if (release.productInfo) {
+            console.group('%c%s','font: 16px sans-serif; color: orangered;',release.publishedContent.properties.seo.slug)
 
+            let slug = {
+                slug: release.publishedContent.properties.seo.slug,
+                title: release.publishedContent.properties.coverCard.properties.title,
+                model: []
+            } 
+            console.log('%c%s','font: 12px sans-serif;',release.publishedContent.properties.coverCard.properties.title)
 
+            
+                release.productInfo.forEach((product)=> {
+                    
+                    console.groupCollapsed('%c%s','font: bold 14px sans-serif;',product.merchProduct.labelName)
+                    
+                    let model = {
+                        modelName: product.merchProduct.labelName,
+                        id: product.merchProduct.id,
+                        imageURL: product.imageUrls.productImageUrl,
+                        size: [],
+                        stock: []
+                    }
+
+                    console.log('%c%s','font: 12px sans-serif;',product.imageUrls.productImageUrl)
+                    console.log('%c%s','font: 12px sans-serif;',product.merchProduct.id)
+        
+                    console.groupCollapsed('%c%s','font: bold 13px sans-serif;','sku - size')
+        
+                    product.skus.forEach((sizeId)=> {
+                        let size = {}
+                        size[sizeId.nikeSize] = sizeId.id
+                        model.size.push(size)
+
+                        console.log('%c%s %c- %s', 'font: bold 12px sans-serif; color: #FFC500;', sizeId.nikeSize, 'font: 12px sans-serif;', sizeId.id)
+                    })
+                    console.groupEnd()
+        
+                    console.groupCollapsed('%c%s','font: bold 13px sans-serif;','sku - stock')
+        
+                    product.availableSkus.forEach((skuStock)=> {
+                        let stock = {}
+                        stock[skuStock.skuId] = skuStock.level
+                        model.stock.push(stock)
+                        
+                        console.log('%c%s %c- %s', 'font: bold 12px sans-serif; color: #FFC500;', skuStock.skuId, 'font: 12px sans-serif;', skuStock.level); 
+                    })
+
+                    slug.model.push(model)
+                    console.groupEnd()
+                    console.groupEnd()
+                })
+            console.groupEnd()
+            store.slug.push(slug)
+        }
+    })    
+    localStorage.setItem('store', JSON.stringify(store))
+})
---
+++ popup.md
@@ -0,0 +1,2 @@
+## Example:
+![alt text](https://i.imgur.com/o6OOsmZ.png)
