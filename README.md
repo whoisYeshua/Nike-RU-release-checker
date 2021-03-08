004b6babc97ba2b41f297c9f6bb646d302d1a103
whoisYeshua <alien1998@yandex.ru>
2021-03-08 14:17:10 +0300

    Notified of deprecation

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
+# Deprecated!
+
+Due to new Nike CORS policy this solution has been deprecated, so I created [node solution](https://github.com/whoisYeshua/nike-release-checker).
+
 # Nike RU release checker
 
 ## About:
-Script that accesses the Nike API to get product information from the main feed and stores it at localstorage. 
+
+Script that accesses the Nike API to get product information from the main feed and stores it at localstorage.
 Then information outputs to the console, and can also be displayed if you paste correct product-URL.
 
 This code can be adapted to other regions and to a larger storage capacity.
+
 ## Example:
+
 ```
 let url = new URL ('https://api.nike.com/product_feed/threads/v2/')
        url.searchParams.append('count','14')
@@ -13,12 +20,16 @@ let url = new URL ('https://api.nike.com/product_feed/threads/v2/')
        url.searchParams.append('filter','language(ru)')
        url.searchParams.append('filter','channelId(010794e5-35fe-4e32-aaff-cd2c74f89d61)')
 ```
-If you need to increase the number of cached products simply increase `count` param, also, if you need to change 
+
+If you need to increase the number of cached products simply increase `count` param, also, if you need to change
 the region, select the appropriate one at `marketplace()` & `language()`
+
 ## Console preview:
+
 ![Imgur](https://i.imgur.com/PUr6gIS.jpg)
 
 ## UI preview:
+
 [**Demo**](https://nike-ru-release-checker-by-whoisyeshua.netlify.app/)
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/d7892ce3-2f26-46e4-ae7d-f7e5cadb15a6/deploy-status)](https://app.netlify.com/sites/nike-ru-release-checker-by-whoisyeshua/deploys)
@@ -27,10 +38,10 @@ the region, select the appropriate one at `marketplace()` & `language()`
 
 ### Stock description:
 
-| Color                                     | Description           |
-| :---------------------------------------: | :-------------------: |
-| ![Imgur](https://i.imgur.com/5LKBB7p.png) | Low Stock             |
-| ![Imgur](https://i.imgur.com/sIEiTTj.png) | Medium Stock          |  
-| ![Imgur](https://i.imgur.com/v7V9REI.png) | High Stock            |
-| ![Imgur](https://imgur.com/sg2YD4N.png)   | Not announce          |
-| ![Imgur](https://i.imgur.com/CcWn5d7.png) | OOS                   |
+|                   Color                   | Description  |
+| :---------------------------------------: | :----------: |
+| ![Imgur](https://i.imgur.com/5LKBB7p.png) |  Low Stock   |
+| ![Imgur](https://i.imgur.com/sIEiTTj.png) | Medium Stock |
+| ![Imgur](https://i.imgur.com/v7V9REI.png) |  High Stock  |
+|  ![Imgur](https://imgur.com/sg2YD4N.png)  | Not announce |
+| ![Imgur](https://i.imgur.com/CcWn5d7.png) |     OOS      |
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,6 @@
 checkStorage()
 function checkStorage() {
-
-    let store = localStorage.getItem('store') ? JSON.parse(localStorage.getItem('store')) : null;
+    let store = localStorage.getItem('store') ? JSON.parse(localStorage.getItem('store')) : null
 
     let now = new Date()
     now = new Date(now.getFullYear(), now.getMonth(), now.getDate())
@@ -17,167 +16,146 @@ function updateStorage() {
     let now = new Date()
     now = new Date(now.getFullYear(), now.getMonth(), now.getDate())
 
-    getProductId().then(function(data) {
-
-        let store = {
-            lastFetch: now.toISOString(),
-            products: []
-        }
-        data.objects.forEach((release)=> {
-            if (release.productInfo) {
-
-                let slug = {
-                    slug: release.publishedContent.properties.seo.slug,
-                    title: release.publishedContent.properties.coverCard.properties.title,
-                    models: []
-                }
-
-                release.productInfo.forEach((product)=> {
-
-                    let model = {
-                        modelName: product.merchProduct.labelName,
-                        id: product.merchProduct.id,
-                        imageURL: product.imageUrls.productImageUrl,
-                        sizes: [],
-                        stock: []
+    getProductId()
+        .then(function (data) {
+            let store = {
+                lastFetch: now.toISOString(),
+                products: [],
+            }
+            data.objects.forEach(release => {
+                if (release.productInfo) {
+                    let slug = {
+                        slug: release.publishedContent.properties.seo.slug,
+                        title: release.publishedContent.properties.coverCard.properties.title,
+                        models: [],
                     }
 
-                    product.skus.forEach((sizeId)=> {
-                        let size = {}
-                        size[sizeId.nikeSize] = sizeId.id
-                        model.sizes.push(size)
-                    })
+                    release.productInfo.forEach(product => {
+                        let model = {
+                            modelName: product.merchProduct.labelName,
+                            id: product.merchProduct.id,
+                            imageURL: product.imageUrls.productImageUrl,
+                            sizes: [],
+                            stock: [],
+                        }
 
-                    product.availableSkus.forEach((skuStock)=> {
-                        let stock = {}
-                        stock[skuStock.skuId] = skuStock.level
-                        model.stock.push(stock)
+                        product.skus.forEach(sizeId => {
+                            let size = {}
+                            size[sizeId.nikeSize] = sizeId.id
+                            model.sizes.push(size)
+                        })
+
+                        product.availableSkus.forEach(skuStock => {
+                            let stock = {}
+                            stock[skuStock.skuId] = skuStock.level
+                            model.stock.push(stock)
+                        })
+                        slug.models.push(model)
                     })
-                    slug.models.push(model)
+                    store.products.push(slug)
+                }
+            })
+            localStorage.setItem('store', JSON.stringify(store))
+            output(JSON.parse(localStorage.getItem('store')))
+        })
+        .catch(e => {
+            console.error(e)
+            if (e.message === 'Failed to fetch') {
+                const span = `<span>Due to new Nike CORS policy this method doesn\`t work. Check new solution at link below</span>`
+                const modalBtn = `<a class="btn modal__btn" data-micromodal-close aria-label="Open Repo" href="https://github.com/whoisYeshua/nike-release-checker">Check</a>`
+                document.querySelector('main.modal__content').insertAdjacentHTML('afterbegin', span)
+                document.querySelector('main.modal__content').insertAdjacentHTML('beforeend', modalBtn)
+                MicroModal.show('modal-1', {
+                    onClose: () => (document.querySelector('main.modal__content').innerHTML = ''),
                 })
-                store.products.push(slug)
             }
         })
-        localStorage.setItem('store', JSON.stringify(store))
-        output(JSON.parse(localStorage.getItem('store')))
-    }).catch(e => {
-        console.error(e)
-    })
 }
 
 async function getProductId() {
-    let url = new URL ('https://api.nike.com/product_feed/threads/v2/')
-    url.searchParams.append('count','14')
-    url.searchParams.append('filter','marketplace(RU)')
-    url.searchParams.append('filter','language(ru)')
-    url.searchParams.append('filter','channelId(010794e5-35fe-4e32-aaff-cd2c74f89d61)')
+    let url = new URL('https://api.nike.com/product_feed/threads/v2/')
+    url.searchParams.append('count', '14')
+    url.searchParams.append('filter', 'marketplace(RU)')
+    url.searchParams.append('filter', 'language(ru)')
+    url.searchParams.append('filter', 'channelId(010794e5-35fe-4e32-aaff-cd2c74f89d61)')
 
-    const resp = await fetch(url.href);
-    const text = await resp.text();
-    const json = await JSON.parse(text)
+    try {
+        const resp = await fetch(url.href)
+        const text = await resp.text()
+        const json = await JSON.parse(text)
 
-    if (resp.status === 200) {
-        console.log('%c%s','color: white; background: green; font: 16px sans-serif; padding: 5px;',resp.status);
-        return json
-    } else {
-        console.log('%c%s','color: white; background: red; font: 16px sans-serif; padding: 5px;',resp.status)
-        throw new Error(`Error in fetch - ${resp.status}`)
+        if (resp.status === 200) {
+            console.log('%c%s', 'color: white; background: green; font: 16px sans-serif; padding: 5px;', resp.status)
+            return json
+        } else {
+            console.log('%c%s', 'color: white; background: red; font: 16px sans-serif; padding: 5px;', resp.status)
+            throw new Error(`Error in fetch - ${resp.status}`)
+        }
+    } catch (error) {
+        throw error
     }
 }
 
 function output(store) {
-    store.products.forEach(product=> {
-        console.group('%c%s','font: 16px sans-serif; color: orangered;',product.slug)
+    store.products.forEach(product => {
+        console.group('%c%s', 'font: 16px sans-serif; color: orangered;', product.slug)
 
-        console.log('%c%s','font: 12px sans-serif;',product.title)
+        console.log('%c%s', 'font: 12px sans-serif;', product.title)
 
-        product.models.forEach((model) => {
-          console.groupCollapsed(
-            "%c%s",
-            "font: bold 14px sans-serif;",
-            model.modelName
-          );
+        product.models.forEach(model => {
+            console.groupCollapsed('%c%s', 'font: bold 14px sans-serif;', model.modelName)
 
-          console.log("%c%s", "font: 12px sans-serif;", model.imageURL);
-          console.log("%c%s", "font: 12px sans-serif;", model.id);
+            console.log('%c%s', 'font: 12px sans-serif;', model.imageURL)
+            console.log('%c%s', 'font: 12px sans-serif;', model.id)
 
-          console.groupCollapsed(
-            "%c%s",
-            "font: bold 13px sans-serif;",
-            "size - stock"
-          );
+            console.groupCollapsed('%c%s', 'font: bold 13px sans-serif;', 'size - stock')
 
-          for (let i in model.sizes) {
-            let key = Object.keys(model.sizes[i])[0];
-            let value = model.sizes[i][key];
-            let stock = model.stock[i][value];
+            for (let i in model.sizes) {
+                let key = Object.keys(model.sizes[i])[0]
+                let value = model.sizes[i][key]
+                let stock = model.stock[i][value]
 
-            if (stock === "HIGH") {
-              console.log(
-                "%c%s %c- %s",
-                "font: bold 12px sans-serif;",
-                key,
-                "font: 12px sans-serif; color: green",
-                stock
-              );
-            } else if (stock === "MEDIUM") {
-              console.log(
-                "%c%s %c- %s",
-                "font: bold 12px sans-serif;",
-                key,
-                "font: 12px sans-serif; color: #FFC500",
-                stock
-              );
-            } else if (stock === "LOW") {
-              console.log(
-                "%c%s %c- %s",
-                "font: bold 12px sans-serif;",
-                key,
-                "font: 12px sans-serif; color: red",
-                stock
-              );
-            } else {
-              console.log(
-                "%c%s %c- %s",
-                "font: bold 12px sans-serif;",
-                key,
-                "font: 12px sans-serif;",
-                stock
-              );
+                if (stock === 'HIGH') {
+                    console.log('%c%s %c- %s', 'font: bold 12px sans-serif;', key, 'font: 12px sans-serif; color: green', stock)
+                } else if (stock === 'MEDIUM') {
+                    console.log('%c%s %c- %s', 'font: bold 12px sans-serif;', key, 'font: 12px sans-serif; color: #FFC500', stock)
+                } else if (stock === 'LOW') {
+                    console.log('%c%s %c- %s', 'font: bold 12px sans-serif;', key, 'font: 12px sans-serif; color: red', stock)
+                } else {
+                    console.log('%c%s %c- %s', 'font: bold 12px sans-serif;', key, 'font: 12px sans-serif;', stock)
+                }
             }
-          }
-          console.groupEnd();
-          console.groupEnd();
-        });
-        console.groupEnd();
+            console.groupEnd()
+            console.groupEnd()
+        })
+        console.groupEnd()
     })
 }
 
-document.getElementById('check').addEventListener('click', function (event){
+document.getElementById('check').addEventListener('click', function (event) {
     let url = document.getElementById('url').value
 
     if (url) {
         let productSlug = url.slice(33)
         showProduct(productSlug)
-
-    } else {errorMsg('Past correct URL')}
+    } else {
+        errorMsg('Past correct URL')
+    }
 })
 
-
 function showProduct(productSlug) {
-    let store = localStorage.getItem('store') ? JSON.parse(localStorage.getItem('store')) : null;
-    let existing;
+    let store = localStorage.getItem('store') ? JSON.parse(localStorage.getItem('store')) : null
+    let existing
 
     if (store) {
         store.products.forEach(product => {
             if (product.slug === productSlug) {
-                let title = product.title;
-                existing = true;
+                let title = product.title
+                existing = true
 
                 if (product.models.length > 1) {
-
-                    product.models.forEach(model=> {
-                        let modalBtn;
+                    product.models.forEach(model => {
+                        let modalBtn
 
                         if (model.modelName.includes('GS')) {
                             modalBtn = `<button class="modal__btn" data-model="${model.modelName}" data-micromodal-close aria-label="Choose size chart">GS</button>`
@@ -192,29 +170,27 @@ function showProduct(productSlug) {
                         document.querySelector('main.modal__content').insertAdjacentHTML('afterbegin', modalBtn)
                     })
 
-                    MicroModal.show('modal-1',{
-                        onClose: () => document.querySelector('main.modal__content').innerHTML = ''
-                    });
+                    MicroModal.show('modal-1', {
+                        onClose: () => (document.querySelector('main.modal__content').innerHTML = ''),
+                    })
 
-                    document.querySelectorAll(".modal__btn").forEach((btn) => {
-                      btn.addEventListener("click", () => {
-                          product.models.forEach(model => {
-                              if (model.modelName === btn.dataset.model) {
-                                  createProductInfo(model)
-                              }
-                          })
-                      });
-                    });
-
+                    document.querySelectorAll('.modal__btn').forEach(btn => {
+                        btn.addEventListener('click', () => {
+                            product.models.forEach(model => {
+                                if (model.modelName === btn.dataset.model) {
+                                    createProductInfo(model)
+                                }
+                            })
+                        })
+                    })
                 } else {
                     createProductInfo(product.models[0])
                 }
 
                 function createProductInfo(model) {
-
-                    let modelName = model.modelName;
-                    let productId = model.id;
-                    let image = model.imageURL;
+                    let modelName = model.modelName
+                    let productId = model.id
+                    let image = model.imageURL
 
                     let productInfo = `<div class="product-info">`
                     productInfo += `<h1 class="subtitle">${modelName}</h1>`
@@ -224,15 +200,15 @@ function showProduct(productSlug) {
                     productInfo += `<div class="sizes">`
 
                     for (let i in model.sizes) {
-                        let size = Object.keys(model.sizes[i])[0];
-                        let value = model.sizes[i][size];
-                        let stock = model.stock[i][value];
+                        let size = Object.keys(model.sizes[i])[0]
+                        let value = model.sizes[i][size]
+                        let stock = model.stock[i][value]
 
-                        if (stock === "HIGH") {
+                        if (stock === 'HIGH') {
                             stock = 'high'
-                        } else if (stock === "MEDIUM") {
+                        } else if (stock === 'MEDIUM') {
                             stock = 'medium'
-                        } else if (stock === "LOW") {
+                        } else if (stock === 'LOW') {
                             stock = 'low'
                         } else if (stock === 'OOS') {
                             stock = 'oos'
@@ -245,7 +221,6 @@ function showProduct(productSlug) {
                         productInfo += `<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">`
                         productInfo += `<circle class="${stock}" cx="5" cy="5" r="5"/>`
                         productInfo += `</svg></div>`
-
                     }
                     productInfo += `</div></div>`
                     if (document.querySelector('.output-area p') !== null) {
@@ -259,42 +234,42 @@ function showProduct(productSlug) {
                             show(document.querySelector('.product-info'))
                         }, 400)
                     }
-
                 }
-
             }
         })
         if (!existing) {
             updateStorage()
-            errorMsg('Couldn\`t find this product, try again')
+            errorMsg('Couldn`t find this product, try again')
         }
-    } else {errorMsg('Something wrong with data, check localStorage')}
+    } else {
+        errorMsg('Something wrong with data, check localStorage')
+    }
 }
 
-function errorMsg (msg) {
+function errorMsg(msg) {
     let error = document.createElement('p')
-    error.innerHTML = msg;
-    error.classList.add("error", "show")
+    error.innerHTML = msg
+    error.classList.add('error', 'show')
     document.querySelector('.output-area').prepend(error)
-    setTimeout(()=> {
+    setTimeout(() => {
         hideErr(error)
     }, 2500)
 }
 
 function hideErr(error) {
-    error.classList.add("hide");
-    setTimeout(()=> {
-        error.remove();
+    error.classList.add('hide')
+    setTimeout(() => {
+        error.remove()
     }, 400)
 }
 
 function hideBlock(block) {
-    document.querySelector(`${block}`).classList.add("hide");
-    setTimeout(()=> {
-        document.querySelector(`${block}`).remove();
+    document.querySelector(`${block}`).classList.add('hide')
+    setTimeout(() => {
+        document.querySelector(`${block}`).remove()
     }, 400)
 }
 
 function show(block) {
-    block.classList.add("show");
+    block.classList.add('show')
 }
