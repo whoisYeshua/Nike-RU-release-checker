8488ac3ede57540d17f320378c33b4db15702918
whoisYeshua <alien1998@yandex.ru>
2021-11-27 14:01:06 +0300

    Create separated userAgent checker

---
+++ scripts/userAgent.js
@@ -0,0 +1,30 @@
+let outputBlock = document.querySelector('.main__output')
+
+const SHOURTCUT_TAUPLES = {
+  Win32: {
+    Chrome:
+      '<p>or check console <kbd>Ctrl</kbd> + <kbd style="padding: 6px 16px">Shift</kbd> + <kbd style="padding: 6px 12px">j</kbd></p>',
+    Firefox:
+      '<p>or check console <kbd>Ctrl</kbd> + <kbd style="padding: 6px 16px">Shift</kbd> + <kbd style="padding: 6px 12px">j</kbd></p>',
+  },
+  MacIntel: {
+    Chrome:
+      '<p>or check console <kbd>⌥</kbd> + <kbd>⌘</kbd> + <kbd style="padding: 6px 12px">j</kbd></p>',
+    Firefox:
+      '<p>or check console <kbd>⌘</kbd> + <kbd style="padding: 6px 16px">⇧ Shift</kbd> + <kbd style="padding: 6px 12px">j</kbd></p>',
+    Safari:
+      '<p>or check console <kbd>⌥</kbd> + <kbd>⌘</kbd> + <kbd style="padding: 6px 12px">c</kbd></p>',
+  },
+}
+
+const mobileRegexp =
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|SamsungBrowser/i
+
+const browserRegexp = /(?<browser>Chrome|Safari|Firefox)\/?\s*(\d+)/
+
+const isMobile = mobileRegexp.test(navigator.userAgent)
+const isKnownBrowser = navigator.userAgent.match(browserRegexp)?.groups?.browser
+
+if (!isMobile && isKnownBrowser) {
+  outputBlock.innerHTML = SHOURTCUT_TAUPLES[navigator.platform][isKnownBrowser]
+}
